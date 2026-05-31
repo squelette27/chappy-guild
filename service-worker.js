523fe525e23d22274b492e17ee043e497e919e0e
squelette27 <suklet0207@gmail.com>
2026-05-31 18:34:22 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-const C='chappy-guild-v06';const A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./celestria.png','./riona.png','./mina.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))) });self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='chappy-guild-v08';
+const A=[
+'./','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+'./assets/backgrounds/bg_tavern_main.png',
+'./assets/characters/ch_celestria.png','./assets/characters/ch_riona.png','./assets/characters/ch_mina.png',
+'./assets/npc/npc_merchant.png','./assets/npc/npc_noble.png','./assets/npc/npc_mage.png','./assets/npc/npc_rookie.png'
+];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
